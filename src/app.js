91865248bc8ac1d4dde69d7d8b89ca609583301e
Jonathan Caliva <cjandy.jc@gmail.com>
2022-11-03 13:48:19 -0300

    back

--- src/app.js
+++ src/app.js
@@ -15,11 +15,7 @@ const server = express()
 server.name = 'API';
 server.use(express.json())
 
-server.use(cors([
-  {
-      origin: "*"
-  }
-]))
+server.use(cors())
 server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", 'true');
