583270361bc8f7f9fea322809a3d64d63b171634
Jonathan Caliva <cjandy.jc@gmail.com>
2022-11-03 12:49:50 -0300

    back actualizacion

--- src/app.js
+++ src/app.js
@@ -4,19 +4,41 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
+const dotenv = require('dotenv')
 
 
-require('./db.js');
+// require('./db.js');
 
-const server = express();
+// const server = express();
 
-server.name = 'API';
+// server.name = 'API';
 
-server.use(express.json())
+// server.use(express.json())
 
-server.use(cors());
+// server.use(cors());
+
+const server = express()
+
+dotenv.config()
+
+server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
+server.use(bodyParser.json({
+  limit: '50mb',
+  verify: (req, res, buf) => {
+    req.rawBody = buf
+  }
+}))
+server.use(cookieParser())
+server.use(morgan('dev'))
+server.use(cors([
+  {
+      origin: "https://pi-dogs-sooty.vercel.app", //servidor que deseas que consuma o (*) en caso que sea acceso libre
+      credentials: true
+  }
+]))
 server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", 'true');
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
